18c1ff0b91f153089c12903bbeabb50d3d7cb8ce
Rael <156195134+rael-ph@users.noreply.github.com>
2024-01-19 15:32:50 -0300

    Update qr-code.py

--- qr-code.py
+++ qr-code.py
@@ -4,19 +4,48 @@ import qrcode
 from pathlib import Path
 from datetime import datetime
 
-
 def qrcode_read():
-    camera_id = 0
-    delay = 1
-    window_name = 'QR Code'
+    choice = input("Ler QR code:\nC - câmera\nD - diretório\nEscolha: ")
 
-    qrcd = cv2.QRCodeDetector()
-    cap = cv2.VideoCapture(camera_id)
+    if choice.upper() == "C":
+        camera_id = 0
+        delay = 1
+        window_name = 'QR Code'
 
-    while True:
-        ret, frame = cap.read()
-        if ret:
+        qrcd = cv2.QRCodeDetector()
+        cap = cv2.VideoCapture(camera_id)
+
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                ret_qrcd, decoded_info, points, _ = qrcd.detectAndDecodeMulti(frame)
+                if ret_qrcd:
+                    for s, p in zip(decoded_info, points):
+                        if s:
+                            print("QRCODE", s)
+                            color = (0, 255, 0)
+                            frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
+
+                    cv2.imshow(window_name, frame)
+                    cv2.waitKey(2000)
+                    break
+
+                cv2.imshow(window_name, frame)
+            if cv2.waitKey(delay) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
+        cap.release()
+
+    elif choice.upper() == "D":
+        # Escolher o arquivo de imagem
+        file_path = input("Caminho completo do arquivo de imagem: ")
+        
+        if os.path.exists(file_path):
+            frame = cv2.imread(file_path)
+            qrcd = cv2.QRCodeDetector()
             ret_qrcd, decoded_info, points, _ = qrcd.detectAndDecodeMulti(frame)
+
             if ret_qrcd:
                 for s, p in zip(decoded_info, points):
                     if s:
@@ -24,20 +53,18 @@ def qrcode_read():
                         color = (0, 255, 0)
                         frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
 
-                cv2.imshow(window_name, frame)
+                cv2.imshow("QR Code", frame)
                 cv2.waitKey(2000)
-                break
-
-            cv2.imshow(window_name, frame)
-        if cv2.waitKey(delay) & 0xFF == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
-    cap.release()
+            else:
+                print("Nenhum QR code detectado na imagem.")
+            
+            cv2.destroyAllWindows()
+        else:
+            print("Caminho do arquivo inválido.")
 
 
 def qrcode_create():
-    text = input("Digite o texto para gerar o QR code: ")
+    text = input("Texto para gerar o QR code: ")
 
     # Criar o QR code
     qr = qrcode.QRCode(
